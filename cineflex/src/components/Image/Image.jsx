6adc3b21c804bb35acf7309f2b311b255224698a
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-12-11 17:23:33 +0530

    Button and image component added

--- cineflex/src/components/Image/Image.jsx
+++ cineflex/src/components/Image/Image.jsx
@@ -1,5 +1,23 @@
-const Image = () => {
-  return <div>Image</div>;
+import PropTypes from "prop-types";
+import styles from "./Image.module.css";
+import BrokenImage from "../../assets/blank-image";
+
+const Image = ({ src, alt, className }) => {
+  const BrokenImageHandler = (error) => {
+    error.target.src = BrokenImage;
+  };
+
+  return (
+    <div className={styles[className]}>
+      <img src={src} alt={alt} onError={BrokenImageHandler} />
+    </div>
+  );
+};
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export default Image;
