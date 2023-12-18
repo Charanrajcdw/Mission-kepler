95791abf751d10f84f08d330151c13a18df514c5
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-12-19 00:26:45 +0530

    Not found page completed

--- cineflex/src/pages/NotFound/NotFound.jsx
+++ cineflex/src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./NotFound.module.css";
+import Button from "../../components/Button/Button";
+import { NOT_FOUND, ROUTE_PATHS } from "../../constants";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const redirectToHome = () => {
+    navigate(ROUTE_PATHS.home);
+  };
+
+  return (
+    <div className={styles["not-found-container"]}>
+      <p>{NOT_FOUND.title}</p>
+      <Button clickHandler={redirectToHome}>{NOT_FOUND.button}</Button>
+    </div>
+  );
 };
 
 export default NotFound;
